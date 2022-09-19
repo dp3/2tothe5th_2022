132d7efcfbf78f938c4d44bbf4743ed40fd5d3a2
Devon <Devon Peel>
2022-09-19 09:02:59 -0500

    adding in mlb_score.sh

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Cerner 2^5 2022
 
 Day 1: bash_parallel.sh
-`
+Day 2: wordle.py 
+Day 3: mlb_scores.sh
---
+++ mlb_scores.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "-------------------------------------------"
+curl -s 'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1' | jq -r '.dates | .[] | .games | .[] | .teams' | egrep 'home|away|name|score|detailedState' | sed  's/\"away\":\ {/\n-------------------------------------------\n/g' | sed 's/\"home\":\ {//g' | awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' | awk 'NR%2{printf "%s ",$0;next;}1' | sed 's/\"//g'  | sed 's/\ //g' | sed 's/name://g' | sed 's/score:/:\ /g' | sed 's/.$//g' | sed 's/,/:\ /g' 
+
+
+#update every 2 seconds 
+sleep 2
+watch -d sh mlb_scores.sh
