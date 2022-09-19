f4aae3bfbb0dea8f8851389f4e0f1d1508a3b060
Devon <Devon Peel>
2022-09-19 09:26:56 -0500

    adding descriptions in the readme  and #cerner_2tothe5th_2022 in the mlb_scores.sh script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Cerner 2^5 2022
 
-Day 1: bash_parallel.sh
-Day 2: wordle.py 
-Day 3: mlb_scores.sh
+Day 1: bash_parallel.sh - multithreaded alphabet print a-az each letter's print in a new thread 
+
+Day 2: wordle.py - wordle clone in python  
+
+Day 3: mlb_scores.sh - gathers and prints the mlb scores in obfuscated fashion
--- mlb_scores.sh
+++ mlb_scores.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+#cerner_2tothe5th_2022
 echo "-------------------------------------------"
 curl -s 'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1' | jq -r '.dates | .[] | .games | .[] | .teams' | egrep 'home|away|name|score|detailedState' | sed  's/\"away\":\ {/\n-------------------------------------------\n/g' | sed 's/\"home\":\ {//g' | awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' | awk 'NR%2{printf "%s ",$0;next;}1' | sed 's/\"//g'  | sed 's/\ //g' | sed 's/name://g' | sed 's/score:/:\ /g' | sed 's/.$//g' | sed 's/,/:\ /g' 
 
